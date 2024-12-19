77803e44636511c3d1171effc647179f777960ce
Zakharov_Rostislav <r.z.8102.80@gmail.com>
2024-12-19 16:48:04 +0400

    fix completed

--- main.py
+++ main.py
@@ -13,6 +13,87 @@ data = [
       "image": "https://example.com/images/frodo.jpg"
     }
   },
+  {
+    "id": "2",
+    "attributes": {
+      "name": "Сэмвайз Гэмджи",
+      "born": "6 апреля 2980 Третьей Эпохи",
+      "died": "Неизвестно (покинул Средиземье в Четвертой Эпохе)",
+      "image": "https://example.com/images/sam.jpg"
+    }
+  },
+  {
+    "id": "3",
+    "attributes": {
+      "name": "Арагорн",
+      "born": "1 марта 2931 Третьей Эпохи",
+      "died": "1 марта 120 Четвертой Эпохи",
+      "image": "https://example.com/images/aragorn.jpg"
+    }
+  },
+  {
+    "id": "4",
+    "attributes": {
+      "name": "Гэндальф",
+      "born": "Неизвестно (известен как Майар Олорин)",
+      "died": "Не применимо (бессмертен)",
+      "image": "https://example.com/images/gandalf.jpg"
+    }
+  },
+  {
+    "id": "5",
+    "attributes": {
+      "name": "Леголас",
+      "born": "Неизвестно (синдарский эльф)",
+      "died": "Неизвестно (покинул Средиземье с Гимли)",
+      "image": "https://example.com/images/legolas.jpg"
+    }
+  },
+  {
+    "id": "6",
+    "attributes": {
+      "name": "Гимли",
+      "born": "2879 Третьей Эпохи",
+      "died": "Неизвестно (покинул Средиземье с Леголасом)",
+      "image": "https://example.com/images/gimli.jpg"
+    }
+  },
+  {
+    "id": "7",
+    "attributes": {
+      "name": "Боромир",
+      "born": "2978 Третьей Эпохи",
+      "died": "26 февраля 3019 Третьей Эпохи",
+      "image": "https://example.com/images/boromir.jpg"
+    }
+  },
+  {
+    "id": "8",
+    "attributes": {
+      "name": "Галадриэль",
+      "born": "Неизвестно (одна из первых эльфов Средиземья)",
+      "died": "Не применимо (покинула Средиземье в Четвертой Эпохе)",
+      "image": "https://example.com/images/galadriel.jpg"
+    }
+  },
+  {
+    "id": "9",
+    "attributes": {
+      "name": "Саурон",
+      "born": "Неизвестно (известен как Майар)",
+      "died": "Третья Эпоха 3019 (утрата физического тела)",
+      "image": "https://example.com/images/sauron.jpg"
+    }
+  },
+  {
+    "id": "10",
+    "attributes": {
+      "name": "Голлум (Смеагол)",
+      "born": "около 2430 Третьей Эпохи",
+      "died": "25 марта 3019 Третьей Эпохи",
+      "image": "https://example.com/images/gollum.jpg"
+    }
+  }
 ]
 
 
