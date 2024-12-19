e95ddbc97531a47cedd60b6c90780a58f3ea639e
Zakharov_Rostislav <r.z.8102.80@gmail.com>
2024-12-19 16:26:23 +0400

    fix

--- main.py
+++ main.py
@@ -5,70 +5,70 @@ app = FastAPI()
 
 data = [
   {
-    "id": 1,
+    "id": "1",
     "name": "Фродо Бэггинс",
     "born": "22 сентября 2968 Третьей Эпохи",
     "died": "Неизвестно (покинул Средиземье в Четвертой Эпохе)",
     "image": "https://example.com/images/frodo.jpg"
   },
   {
-    "id": 2,
+    "id": "2",
     "name": "Гэндальф Серый",
     "born": "До начала времён",
     "died": "Неизвестно (становление Гэндальфом Белым)",
     "image": "https://example.com/images/gandalf.jpg"
   },
   {
-    "id": 3,
+    "id": "3",
     "name": "Арагорн",
     "born": "1 марта 2931 Третьей Эпохи",
     "died": "1 марта 120 Четвертой Эпохи",
     "image": "https://example.com/images/aragorn.jpg"
   },
   {
-    "id": 4,
+    "id": "4",
     "name": "Леголас",
     "born": "Неизвестно (Третья Эпоха)",
     "died": "Неизвестно (покинул Средиземье после Войны Кольца)",
     "image": "https://example.com/images/legolas.jpg"
   },
   {
-    "id": 5,
+    "id": "5",
     "name": "Гимли",
     "born": "2879 Третьей Эпохи",
     "died": "Неизвестно (покинул Средиземье вместе с Леголасом)",
     "image": "https://example.com/images/gimli.jpg"
   },
   {
-    "id": 6,
+    "id": "6",
     "name": "Сэмвайз Гэмджи",
     "born": "6 апреля 2980 Третьей Эпохи",
     "died": "Неизвестно (покинул Средиземье в Четвертой Эпохе)",
     "image": "https://example.com/images/samwise.jpg"
   },
   {
-    "id": 7,
+    "id": "7",
     "name": "Боромир",
     "born": "2978 Третьей Эпохи",
     "died": "26 февраля 3019 Третьей Эпохи",
     "image": "https://example.com/images/boromir.jpg"
   },
   {
-    "id": 8,
+    "id": "8",
     "name": "Голлум (Смеагол)",
     "born": "около 2430 Третьей Эпохи",
     "died": "25 марта 3019 Третьей Эпохи",
     "image": "https://example.com/images/gollum.jpg"
   },
   {
-    "id": 9,
+    "id": "9",
     "name": "Элронд",
     "born": "около 532 Первой Эпохи",
     "died": "Неизвестно (покинул Средиземье в Четвертой Эпохе)",
     "image": "https://example.com/images/elrond.jpg"
   },
   {
-    "id": 10,
+    "id": "10",
     "name": "Арвен Ундомиэль",
     "born": "241 Третьей Эпохи",
     "died": "121 Четвертой Эпохи",
